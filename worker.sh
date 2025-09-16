3b7e8147b3609fdfe2e9e6a931827565c784f9ec
Moritz Kastler <moritz.kastler@outlook.com>
2025-09-16 22:41:26 +0200

    Changed Logging Paths for Worker jobs

--- worker.sh
+++ worker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=TuneWorker # specify the job name for monitoring
-#SBATCH --output=transformer-out/JOB_%j_finetuning_worker.out # specify the output file
-#SBATCH --error=transformer-err/JOB_%j_finetuning_worker.err # specify the error file
+#SBATCH --output=transformer-out/workers/JOB_%j_finetuning_worker.out # specify the output file
+#SBATCH --error=transformer-err/workers/JOB_%j_finetuning_worker.err # specify the error file
 #SBATCH --nodes=1 # As we have single node it should be always set as 1
 #SBATCH --cpus-per-task=10 # Number of CPUs
 #SBATCH --gres=gpu:nvidia_a100_80gb_pcie_3g.39gb:1  # Allocate 1 GPU resources with specified configurations
